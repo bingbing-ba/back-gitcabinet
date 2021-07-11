cfdbb114cc029007aa6efc5f0b8b27b23b64270e
bing <edubing.hphk@gmail.com>
2021-07-12 02:14:50 +0900

    id도 받을 수 있게 request.body 전체를 comments 컬렉션에다가 쓰도록 수정

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,17 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import * as _cors from "cors";
+import * as functions from 'firebase-functions'
+import * as admin from 'firebase-admin'
+import * as _cors from 'cors'
 
-admin.initializeApp();
-const cors = _cors({ origin: true });
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp()
+const cors = _cors({ origin: true })
 
 export const comments = functions.https.onRequest((request, response) => {
   cors(request, response, async () => {
-    if (request.method === "POST") {
-      await admin
-        .firestore()
-        .collection("comments")
-        .add({ content: request.body });
-      response.json({ status: "ok" });
+    if (request.method === 'POST') {
+      await admin.firestore().collection('comments').add(request.body)
+      response.json({ status: 'ok' })
     } else {
-      response.status(405).json({ status: 405 });
+      response.status(405).json({ status: 405 })
     }
-  });
-});
+  })
+})
